16510b16d38771a3047a7565d282db0bf14a1584
Sean Staples <seantstaples.s@gmail.com>
2019-08-14 10:04:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  if (string === lowercase) {
+    return "I can't hear you!"
+  } else if (string === uppercase) {
+    return "YES INDEED!"
+  } else {
+    return "I love you, too"
+  }
+}
